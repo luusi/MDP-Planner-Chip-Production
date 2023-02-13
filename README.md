63169f686f31fce05b19a89db6e31c47983b8868
luusi <luciana.silo@gmail.com>
2023-02-13 12:08:26 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,147 @@
-# MDP-Planner-Chip-Production
+# Stochastic Service Composition
+
+Implementation of Digital Twins Composition in Smart Manufacturing via Markov Decision Processes.
+
+The following sections are:
+- Preliminaries
+- Instructions to reproduce the experiments
+
+
+## Preliminaries
+
+We assume the review uses a UNIX-like machine and that has Python 3.8 installed.
+
+- Set up the virtual environment. 
+First, install [Pipenv](https://pipenv-fork.readthedocs.io/en/latest/).
+Then:
+```
+pipenv install --dev
+```
+                    
+- this command is to start a shell within the Python virtual environment (to be done whenever a new terminal is opened):
+```
+pipenv shell
+```
+
+- Install the Python package in development mode:
+```
+pip install -e .
+# alternatively:
+# python setup.py develop 
+```
+
+- To use rendering functionalities, you will also need to install Graphviz. 
+  At [this page](https://www.graphviz.org/download/) you will
+  find the releases for all the supported platform.
+
+- Generate Python client from OpenAPI v3.0 specification:
+```
+./scripts/generate-openapi-client.sh
+```
+
+## Instructions to reproduce the experiments
+
+Each of the following commands must be run on a separate terminal with the virtual environment activated.
+
+- Run the HTTP server that acts as service repository and communication middleware:
+```
+cd local/things_api
+python app.py
+```
+
+- Run all the services and the target service:
+```
+cd local
+python ./launch_devices.py
+```
+
+- Run the orchestrator
+```
+cd local
+python main.py
+```
+
+### Transition Function Evaluation 
+
+- The transition function changes: as the use of the machine increases, its state of wear increases.
+
+- Every machine starts from a low broken probability (0.05) and a low cost to perform a certain action (-1); at each iteration the broken probability increases by 0.05 and the cost increases by 1.
+
+- We show an output of the painting service transition function before and after the service performs _painting_ action.
+```
+Transition function for service painting_service has changed! 
+Old: {'available': {'painting': [{'done': 0.95, 'broken': 0.05}, -1]}, 'broken': {'check_painting': [{'available': 1}, -10]}, 'done': {'check_painting': [{'available': 1}, 0]}},
+
+New: {'available': {'painting': [{'done': 0.9, 'broken': 0.1}, -2.0]}, 'broken': {'check_painting': [{'available': 1}, -10]}, 'done': {'check_painting': [{'available': 1}, 0]}}
+```
+
+### Policy Evaluation
+
+- Since at each iteration the transition function change due to increased wear of the machines, also the optimal policy is recalculated.
+
+- We have two services that perform painting action: the machine one and the human one.
+
+- At the beginning painting machine is chosen because it is not yet worn. 
+
+- In the early iterations, although the broken probability and the cost are increasing, they are not so important to make the optimal policy change. For the orchestrator will be more convenient choose the machine service over the human (see previous output). 
+  
+- The human painting service has no possibility to break but has high cost of performing the action (-5). 
+
+- At a certain point broken probability of painting machine became 0.2 and cost of performing the action -4, so the optimal policy change and the orchestrator choose human service because is more convenient than machine one.
+```
+Transition function for service painting_service has changed!
+Old: {'available': {'painting': [{'done': 0.85, 'broken': 0.15000000000000002}, -3.0]}, 'broken': {'check_painting': [{'available': 1}, -10]}, 'done': {'check_painting': [{'available': 1}, 0]}},
+New: {'available': {'painting': [{'done': 0.8, 'broken': 0.2}, -4.0]}, 'broken': {'check_painting': [{'available': 1}, -10]}, 'done': {'check_painting': [{'available': 1}, 0]}}
+ ```
+- We show the output of the main regarding the change in the calculation of new policy (for reasons of space we omit the other states):
+```
+old_policy: in state ((
+  'available',
+  'available',
+  'available',
+  'available',
+  'available',
+  'available',
+  'available',
+  'available',
+  'available'),
+ 's9',
+ 'painting'), take action '5'.
+
+new_policy: in state ((
+  'available',
+  'available',
+  'available',
+  'available',
+  'available',
+  'available',
+  'available',
+  'available',
+  'available'),
+ 's9',
+ 'painting'), take action '4'.
+```
+- We observe that the old policy chose for ```painting``` action the service 5 i.e., the painting service, in the calculation of the new policy instead is used service 4 i.e., the human painting service.
+
+- **Note that:** the example that we show in the ceramics production case study happens with the current parameters, all the users who wanted to simulate other case studies could get different results.
+
+## Scheduled maintenance
+
+- Periodically, a scheduled maintenance is implemented for every service. This permits to repair a machine that is broken or simply reset a machine.
+
+- The transition function is restored to the moment when the machine shows no signs of wear.
+
+- We show an output of a scheduled maintenance of a service:
+```
+Restoring transition function for service 'first_baking_service'
+Updating transition function: '{'available': {'first_baking': [{'done': 0.95, 'broken': 0.05}, -1]}, 'broken': {'check_first_baking': [{'available': 1}, -10]}, 'done': {'check_first_baking': [{'available': 1}, 0]}}'
+```
+- In the standard output of the `main.py` process, after `Sending msg for scheduled maintenance`, if the `painting_service`
+  was enough degradated, you will see an `Optimal Policy has changed!` message because its broken probability and cost
+  returned at its original values.
+
+
+Note that it could also happen that the scheduled maintenance does not change the optimal policy because the
+`painting_service` became broken in a previous call of the service and, after transitioning in the `broken` state,
+it was already repaired in the following `check` action, and therefore the maintenance step becomes redundant.
+
---
+++ stochastic_service_composition/composition_mdp.py
@@ -0,0 +1,217 @@
+"""This module implements the algorithm to compute the system-target MDP."""
+from collections import deque
+from typing import Deque, Dict, List, Set, Tuple
+
+from mdp_dp_rl.processes.mdp import MDP
+from pythomata import SimpleDFA
+
+from stochastic_service_composition.services import Service, build_system_service
+from stochastic_service_composition.target import Target
+from stochastic_service_composition.types import Action, State, MDPDynamics
+
+COMPOSITION_MDP_INITIAL_STATE = 0
+COMPOSITION_MDP_INITIAL_ACTION = "initial"
+COMPOSITION_MDP_UNDEFINED_ACTION = "undefined"
+DEFAULT_GAMMA = 0.9
+
+
+def composition_mdp(
+    target: Target, *services: Service, gamma: float = DEFAULT_GAMMA
+) -> MDP:
+    """
+    Compute the composition MDP.
+
+    :param target: the target service.
+    :param services: the community of services.
+    :param gamma: the discount factor.
+    :return: the composition MDP.
+    """
+
+    system_service = build_system_service(*services)
+
+    initial_state = COMPOSITION_MDP_INITIAL_STATE
+    # one action per service (1..n) + the initial action (0)
+    actions: Set[Action] = set(range(len(services)))
+    initial_action = COMPOSITION_MDP_INITIAL_ACTION
+    actions.add(initial_action)
+
+    # add an 'undefined' action for sink states
+    actions.add(COMPOSITION_MDP_UNDEFINED_ACTION)
+
+    transition_function: MDPDynamics = {}
+
+    visited = set()
+    to_be_visited = set()
+    queue: Deque = deque()
+
+    # add initial transitions
+    transition_function[initial_state] = {}
+    initial_transition_dist = {}
+    symbols_from_initial_state = target.policy[target.initial_state].keys()
+    for symbol in symbols_from_initial_state:
+        next_state = (system_service.initial_state, target.initial_state, symbol)
+        next_prob = target.policy[target.initial_state][symbol]
+        initial_transition_dist[next_state] = next_prob
+        queue.append(next_state)
+        to_be_visited.add(next_state)
+    transition_function[initial_state][initial_action] = (initial_transition_dist, 0.0)  # type: ignore
+
+    while len(queue) > 0:
+        current_state = queue.popleft()
+        to_be_visited.remove(current_state)
+        visited.add(current_state)
+        current_system_state, current_target_state, current_symbol = current_state
+
+        transition_function[current_state] = {}
+        # index system symbols (action, service_id) by symbol
+        system_symbols: List[Tuple[Action, int]] = list(
+            system_service.transition_function[current_system_state].keys()
+        )
+        system_symbols_by_symbols: Dict[Action, Set[int]] = {}
+        for action, service_id in system_symbols:
+            system_symbols_by_symbols.setdefault(action, set()).add(service_id)
+
+        for i in system_symbols_by_symbols.get(current_symbol, set()):
+            next_transitions = {}
+            # TODO check if it is needed
+            if current_symbol not in target.transition_function[current_target_state]:
+                continue
+            next_reward = (
+                target.reward[current_target_state][current_symbol]
+                if (current_symbol, i)
+                in system_service.transition_function[current_system_state]
+                else 0
+            )
+            next_target_state = target.transition_function[current_target_state][
+                current_symbol
+            ]
+            next_system_states, next_system_reward = system_service.transition_function[
+                current_system_state
+            ][(current_symbol, i)]
+            for next_symbol, next_prob in target.policy[next_target_state].items():
+                for next_system_state, next_system_prob in next_system_states.items():
+                    next_state = (next_system_state, next_target_state, next_symbol)
+                    if next_prob * next_system_prob == 0.0:
+                        continue
+                    next_transitions[next_state] = next_prob * next_system_prob
+                    if next_state not in visited and next_state not in to_be_visited:
+                        to_be_visited.add(next_state)
+                        queue.append(next_state)
+            transition_function[current_state][i] = (  # type: ignore
+                next_transitions,  # type: ignore
+                next_reward + next_system_reward,
+            )
+
+        # states without outgoing transitions are sink states.
+        # add loop transitions with
+        # - 'undefined' action
+        # - probability 1
+        # - reward 0
+        if len(transition_function[current_state]) == 0:
+            transition_function.setdefault(current_state, {})
+            transition_function[current_state][COMPOSITION_MDP_UNDEFINED_ACTION] = (
+                {current_state: 1.0},
+                0.0,
+            )  # type: ignore
+
+    return MDP(transition_function, gamma)
+
+def comp_mdp(
+    dfa: SimpleDFA, services: Service, gamma: float = DEFAULT_GAMMA
+) -> MDP:
+    """
+    Compute the composition MDP.
+
+    :param target: the target service.
+    :param services: the community of services.
+    :param gamma: the discount factor.
+    :return: the composition MDP.
+    """
+
+    system_service = build_system_service(*services)
+
+    initial_state = COMPOSITION_MDP_INITIAL_STATE
+    # one action per service (1..n) + the initial action (0)
+    actions: Set[Action] = set(range(len(services)))
+    initial_action = COMPOSITION_MDP_INITIAL_ACTION
+    actions.add(initial_action)
+
+    # add an 'undefined' action for sink states
+    actions.add(COMPOSITION_MDP_UNDEFINED_ACTION)
+
+    transition_function: MDPDynamics = {}
+
+    visited = set()
+    to_be_visited = set()
+    queue: Deque = deque()
+
+    # add initial transitions
+
+    # add initial transitions
+    initial_state = (system_service.initial_state, dfa.initial_state)
+    queue.append(initial_state)
+    to_be_visited.add(initial_state)
+    for system_service_state in system_service.states:
+        if system_service_state == system_service.initial_state:
+            continue
+        new_initial_state = (system_service_state, dfa.initial_state)
+        queue.append(new_initial_state)
+        to_be_visited.add(new_initial_state)
+
+    #initial_state = (system_service.initial_state, dfa.initial_state)
+    #queue.append(initial_state)
+    #to_be_visited.add(initial_state)
+    #for system_service_state in system_service.states:
+        #if system_service_state == system_service.initial_state:
+            #continue
+        #new_initial_state = (system_service_state, dfa.initial_state)
+        #queue.append(new_initial_state)
+        #to_be_visited.add(new_initial_state)
+
+    while len(queue) > 0:
+        cur_state = queue.popleft()
+        to_be_visited.remove(cur_state)
+        visited.add(cur_state)
+        cur_system_state, cur_dfa_state = cur_state
+        trans_dist = {}
+
+        next_system_state_trans = system_service.transition_function[
+            cur_system_state
+        ].items()
+
+        # iterate over all available actions of system service
+        # in case symbol is in DFA available actions, progress DFA state component
+        for (symbol, service_id), next_state_info in next_system_state_trans:
+            next_system_state_distr, reward_vector = next_state_info
+            system_reward = reward_vector
+
+            # symbols not in the transition function of the target
+            # are considered as "other"; however, when we add the
+            # LMDP transition, we will label it with the original
+            # symbol.
+            if symbol in dfa.transition_function[cur_dfa_state]:
+                symbol_to_next_dfa_states = dfa.transition_function[cur_dfa_state]
+                next_dfa_state = symbol_to_next_dfa_states[symbol]
+                goal_reward = 1.0 if dfa.is_accepting(next_dfa_state) else 0.0
+            else:
+                next_dfa_state = cur_dfa_state
+                goal_reward = 0.0
+            final_rewards = (goal_reward + system_reward)
+
+            for next_system_state, prob in next_system_state_distr.items():
+                assert prob > 0.0
+                next_state = (next_system_state, next_dfa_state)
+                trans_dist.setdefault((symbol, service_id), ({}, final_rewards))[0][
+                    next_state
+                ] = prob
+                if next_state not in visited and next_state not in to_be_visited:
+                    queue.append(next_state)
+                    to_be_visited.add(next_state)
+
+
+        transition_function[cur_state] = trans_dist
+
+    result = MDP(transition_function, gamma)
+    #result.initial_state = initial_state
+    return result
+
