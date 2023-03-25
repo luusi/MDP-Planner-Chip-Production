d3fe9171a6df0715e2cd4943aaba1d3d292c42b9
luusi <luciana.silo@gmail.com>
2023-03-25 10:54:12 +0100

    Hiding github reference

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup, find_packages
 setup(name='stochastic_service_composition',
       version='0.1.0',
       description='Implementation of stochastic service composition.',
-      url='http://github.com/luusi/stochastic-service-composition',
-      author='Luciana Silo',
-      author_email='silo.1586010@studenti.uniroma1.it',
       license='MIT',
       packages=find_packages(include='stochastic_service_composition*'),
       zip_safe=False,
@@ -20,7 +17,6 @@ setup(name='stochastic_service_composition',
             "pythomata",
             "flask",
             "aiohttp",
-            "aiohttp_jinja2",
-            "mdp_dp_rl @ git+https://github.com/luusi/mdp-dp-rl.git#egg=mdp_dp_rl"
+            "aiohttp_jinja2"
       ]
       )
